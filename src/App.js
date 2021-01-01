7f9cba59b2bf35e5bc230d1574c23381cefec784
sarath <sarathdon9245@gmail.com>
2021-01-01 12:55:04 +0530

    🚀 Chore - moved img to src

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./styles.css";
-import birthdayImg from "/images/birthday.svg";
+import birthdayImg from "./birthday.svg";
 
 export default function App() {
   let [name, setName] = useState("");
